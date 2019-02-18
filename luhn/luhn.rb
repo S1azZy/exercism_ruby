cbd4b3a469c5f890dff4fe110e0536db6e80ab7c
S1azZy <slazzy.main@gmail.com>
2019-02-18 13:29:18 +0300

    add luhn

--- luhn/luhn.rb
+++ luhn/luhn.rb
@@ -1,31 +1,42 @@
 class Luhn
+  def initialize(str)
+    @str = str.delete("\s")
+  end
+
   def self.valid?(str)
-    return false unless valid_string_format?(str)
-
-    digits_from_string(str)
-      .reverse
-      .each_slice(2)
-      .sum { |pair| calculate_pair_sum(pair) }
-      .yield_self { |result| (result % 10).zero? }
+    new(str).perform
   end
 
-  def self.valid_string_format?(str)
-    str = str.gsub(/\s/, '')
+  def perform
+    return false if invalid_string_format?
 
-    str.size > 1 && !str.match?(/[^0-9]/)
+    (checksum % 10).zero?
   end
 
-  def self.digits_from_string(str)
+  private
+
+  attr_reader :str
+
+  def invalid_string_format?
+    str.size <= 1 || str.match?(/[^0-9]/)
+  end
+
+  def digits
     str.scan(/\d/).map(&:to_i)
   end
 
-  def self.calculate_pair_sum(pair)
-    first, second = *pair
-    second = (second || 0) * 2
+  def checksum
+    digits
+      .reverse
+      .each_slice(2)
+      .sum { |pair| calculate_pair_sum(pair) }
+  end
+
+  def calculate_pair_sum(pair)
+    first, second = pair
+    second = second.to_i * 2
     second -= 9 if second > 9
 
     first + second
   end
-
-  private_class_method :calculate_pair_sum, :digits_from_string, :valid_string_format?
 end
